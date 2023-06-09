588069630270f93d2217bf152c0879feee3efa0c
Julian Z <julian.z@gmx.net>
2023-06-10 00:54:13 +0200

    Input Interface (unfinished)

---
+++ becomeyourenemy/Assets/Scripts/Controller/InputInterface.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public interface InputInterface
+{
+
+    protected static Vector2 MoveDirection;
+    protected static bool Ability1;
+    protected static bool Ability2;
+
+}
