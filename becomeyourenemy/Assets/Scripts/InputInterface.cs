77a8fea6b8ff81869eadfd866d697430f0efb47d
Julian Z <julian.z@gmx.net>
2023-06-09 23:50:25 +0200

    Basic Input System + Prototype Scene

---
+++ becomeyourenemy/Assets/Scripts/InputInterface.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    
+    public interface InputInterface
+    {
+
+        protected static Vector2 MoveDirection;
+        protected static bool Ability1;
+        protected static bool Ability2;
+
+    }
+    
+}
