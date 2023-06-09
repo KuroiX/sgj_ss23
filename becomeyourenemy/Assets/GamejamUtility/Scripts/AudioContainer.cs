7792baa523886114276f7fa19e62875a1e1afb7d
Julian Geheeb <geheeb@in.tum.de>
2023-06-09 22:42:23 +0200

    Gamejam utility + input system

---
+++ becomeyourenemy/Assets/GamejamUtility/Scripts/AudioContainer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class AudioContainer : MonoBehaviour
+{
+    public AudioClip BackgroundMusicClip => backgroundMusicClip;
+    
+    [SerializeField] private AudioClip backgroundMusicClip;
+}
---
+++ becomeyourenemy/Assets/GamejamUtility/Scripts/AudioSliderManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSliderManager : MonoBehaviour
+{
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectSlider;
+    [SerializeField] private Slider environmentSlider;
+    
+    private AudioManager _audioManager;
+    
+    private void Start()
+    {
+        _audioManager = FindObjectOfType<AudioManager>();
+        SetSlider();
+    }
+    
+    private void SetSlider()
+    {
+        masterSlider.value = _audioManager.MasterVolume;
+        effectSlider.value = _audioManager.SoundEffectVolume;
+        musicSlider.value = _audioManager.MusicVolume;
+        environmentSlider.value = _audioManager.EnvironmentVolume;
+    }
+    
+    public void OnMasterChanged(float value)
+    {
+        _audioManager.MasterVolume = value;
+    }
+    
+    public void OnEffectChanged(float value)
+    {
+        _audioManager.SoundEffectVolume = value;
+    }
+    
+    public void OnMusicChanged(float value)
+    {
+        _audioManager.MusicVolume = value;        
+    }
+    
+    public void OnEnvironmentChanged(float value)
+    {
+        _audioManager.EnvironmentVolume = value;
+    }
+}
