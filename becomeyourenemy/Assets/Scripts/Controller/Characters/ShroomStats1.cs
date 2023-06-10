d670f95eed5df8848d129c31fcc79f4fa1f02687
J3rry <jerrygamerhd@gmail.com>
2023-06-10 21:45:04 +0200

    refactored frog to shroom

--- becomeyourenemy/Assets/Scripts/Controller/Characters/ShroomStats1.cs
+++ becomeyourenemy/Assets/Scripts/Controller/Characters/ShroomStats1.cs
@@ -3,8 +3,8 @@
 namespace Controller.Characters
 {
     
-    [CreateAssetMenu(fileName = "FrogStats", menuName = "ScriptableObject/Stats/Frog", order = 100)]
-    public class FrogStats1 : DefaultStats
+    [CreateAssetMenu(fileName = "ShroomStats", menuName = "ScriptableObject/Stats/Shroom", order = 100)]
+    public class ShroomStats1 : DefaultStats
     {
         public float lifeTime;
         public GameObject meleeAttack;
