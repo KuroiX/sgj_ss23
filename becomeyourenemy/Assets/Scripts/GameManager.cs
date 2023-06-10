b550d30933f745b7dfa40a309774cbccffd17fcd
J3rry <jerrygamerhd@gmail.com>
2023-06-10 10:45:24 +0200

    timer adjustement

--- becomeyourenemy/Assets/Scripts/GameManager.cs
+++ becomeyourenemy/Assets/Scripts/GameManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float[] fastestCompletionTime;
+    [SerializeField] private int levelAmount;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fastestCompletionTime = new float[levelAmount];
     }
 
     // Update is called once per frame
