16cc267a577b5ba881d34deae59c1e4563490dd2
GeorgeMer <georgemerezas@gmail.com>
2023-06-11 00:07:23 +0200

    Added gost with single projectile

---
+++ becomeyourenemy/Assets/Scripts/Controller/Characters/GostStats1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Controller.Characters
+{
+    
+    [CreateAssetMenu(fileName = "GostStats", menuName = "ScriptableObject/Stats/Gost", order = 100)]
+    public class GostStats1 : DefaultStats
+    {
+        public float velocity;
+        public GameObject projectile;
+    }
+    
+}
