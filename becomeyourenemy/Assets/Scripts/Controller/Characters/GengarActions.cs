0aaef7e6a3463f4d0b97a3ad0487c374ff961743
Julian Z <julian.z@gmx.net>
2023-06-11 10:51:38 +0200

    Fixed Boss AI

--- becomeyourenemy/Assets/Scripts/Controller/Characters/GengarActions.cs
+++ becomeyourenemy/Assets/Scripts/Controller/Characters/GengarActions.cs
@@ -35,16 +35,17 @@ namespace Controller.Characters
             Quaternion lookQuaternion = Quaternion.LookRotation(Vector3.forward, Quaternion.Euler(0, 0, -90) * direction);
 
             // Debug.Log(gameObject + " SHOOTS PROJECTILE at " + direction + "!");
+            Transform parentTransform = transform.parent;
             GameObject projectileInstance = Instantiate(((GengarStats1) stats).projectile,
-                transform.position + new Vector3(direction.x * transform.localScale.x, direction.y * transform.localScale.y),
+                transform.position + new Vector3(direction.x * parentTransform.localScale.x, direction.y * parentTransform.localScale.y),
                 lookQuaternion);
             
             GameObject projectileInstanceUp = Instantiate(((GengarStats1) stats).projectile,
-                transform.position + new Vector3(upDir.x * transform.localScale.x, upDir.y * transform.localScale.y),
+                transform.position + new Vector3(upDir.x * parentTransform.localScale.x, upDir.y * parentTransform.localScale.y),
                 lookQuaternion);
             
             GameObject projectileInstanceDown = Instantiate(((GengarStats1) stats).projectile,
-                transform.position + new Vector3(downDir.x * transform.localScale.x, downDir.y * transform.localScale.y),
+                transform.position + new Vector3(downDir.x * parentTransform.localScale.x, downDir.y * parentTransform.localScale.y),
                 lookQuaternion);
 
             projectileInstance.GetComponent<Projectile>().damage = stats.damage;
